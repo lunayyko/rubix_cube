5befc2151b56643d473d4f03d354a33e21e97976
luna@k7.com <luna@k7.com>
2020-12-13 21:37:43 +0100

    step-2 thought process and python paste

---
+++ step-2/index.js
@@ -0,0 +1,35 @@
+let myArr = [ 'R', 'R', 'W', 'G', 'C', 'W', 'G', 'B', 'B' ];
+console.log(myArr)  
+
+U = () => {
+
+}
+
+switch() {
+    case U:
+        3n
+        +2 %3
+        break;
+    case Ur:
+        3n
+        +1 %3
+        break;
+    case R:
+        3n-2
+        +6 %9
+        break;
+    case Rr:
+        3n
+        +3 %9
+        break;
+    case L:
+        3n
+        +3 %9
+        break;
+    case Lr:
+        3n
+        +6 %9
+        break;
+    
+    default:
+}
